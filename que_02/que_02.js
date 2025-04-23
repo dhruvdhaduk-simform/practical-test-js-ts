6b0428fca830993ca02209760d7344c14f407b37
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-23 15:29:33 +0530

    [refractor que_02]: Add few comments

--- que_02/que_02.js
+++ que_02/que_02.js
@@ -1,26 +1,25 @@
 "use strict";
 
-// // Array Deep Flatten with Depth Limit
-
-// const input = [1, [2, [3, [4, [5]]]]];
-
-// output: [1, 2, 3, [4, [5]]]
-
 function flatten(arr, limit) {
+    // Terminating condition for recursion.
     if (limit <= 0)
         return arr;
 
     const result = [];
 
+    // Iterate over each item in arr.
     for (let i = 0; i < arr.length; i++) {
+        // push each items in current item if it is an array.
         if (Array.isArray(arr[i])) {
             result.push(...arr[i]);
         }
+        // push the current item.
         else {
             result.push(arr[i]);
         }
     }
 
+    // Recursively call flatten with (limit - 1)
     return flatten(result, limit - 1);
 
 }
