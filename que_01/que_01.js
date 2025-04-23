0697433286b68362d6b4df74ae4afc3e3ac2cfe2
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-23 15:26:41 +0530

    [refractor que_01]: Make it generic, handle duplicate items in array, add few comments

--- que_01/que_01.js
+++ que_01/que_01.js
@@ -1,34 +1,36 @@
 "use strict";
 
-// // Create merge function that accepts data like below and merges them by given    customerId
-// [
-//     { customerId: 5, minutes: 45, utensils: ['table'] },
-//     { customerId: 3, minutes: 120, utensils: ['chopsticks'] },
-//     { customerId: 9, minutes: 15, utensils: ['spoon'] },
-//     { customerId: 3, minutes: 90, utensils: ['fork', 'knife'] },
-//     { customerId: 3, minutes: 180, utensils: ['plate'] },
-//     { customerId: 6, minutes: 180, utensils: ['bowl'] },
-//     { customerId: 6, minutes: 180, utensils: ['glass'] },
-// ]
-
-// //result should be like below
-// [
-//     { customerId: 5, minutes: 45, utensils: ['table'] },
-//     { customerId: 3, minutes: 390, utensils: ['chopsticks', 'fork', 'knife', 'plate'] },
-//     { customerId: 9, minutes: 15, utensils: ['spoon'] },
-//     { customerId: 6, minutes: 360, utensils: ['bowl', 'glass'] }
-// ]
-
-function mergeData(data) {
+function mergeData(data, idKey) {
+    // Stored the merged data in array.
     const result = [];
+
+    // Iterate over each entry in data
     for (let i = 0; i < data.length; i++) {
+        // Find the already stored merged item for current entry's id.
         const acc = result.find(obj => obj.customerId === data[i].customerId);
+        // Push the current entry in result if the accumulator doesn't exist.
         if (!acc) {
             result.push({ ...data[i], utensils: [...data[i].utensils] });
             continue;
         }
-        acc.minutes += data[i].minutes;
-        acc.utensils = [...acc.utensils, ...data[i].utensils];
+
+        // Iterate over each key in accumulator to update it.
+        for (const k in acc) {
+            // Do not update the idKey
+            if (k === idKey) continue;
+
+            // Update the number
+            if (typeof acc[k] === 'number') {
+                acc[k] += data[i][k];
+            }
+            // Update the array while handling duplicate items.
+            else if (Array.isArray(acc[k])) {
+                for (const item of data[i][k]) {
+                    if (!acc[k].includes(item))
+                        acc[k].push(item);
+                }
+            }
+        }
     }
     return result;
 }
@@ -44,6 +46,6 @@ const input = [
     { customerId: 6, minutes: 180, utensils: ['glass'] },
 ];
 
-const output = mergeData(input);
+const output = mergeData(input, 'customerId');
 
 console.log(output);
